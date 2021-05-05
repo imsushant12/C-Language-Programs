1395c0079558d438ce3fd50849734c48aecd0818
imsushant12 <sushantgaurav57@gmail.com>
2021-05-06 00:39:10 +0530

    first commit

--- binary to decimal function.c
+++ binary to decimal function.c
@@ -1,11 +1,22 @@
+/*
+AIM - To take binary number from the user 
+      and convert it into binary number.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Function decleration*/
 int BtoD (char s[]);
-main()
+
+int main()
 {
     char z[100];
-    printf("Enter the number");
+    printf("Enter the binary number : ");
     gets(z);
     printf("Decimal equivalent = %d",BtoD(z));
+    return 0;
 }
+/*Function to convert binary to decimal*/
 int BtoD (char s[])
 {
     int i,d=0;
@@ -18,3 +29,16 @@ int BtoD (char s[])
     }
     return(d);
 }
+
+/*
+
+Sample Input/Output:
+
+Enter the binary number : 1111
+Decimal equivalent = 15
+
+
+Time Complexity : O(n)
+Space Complexity : O(n)
+
+*/
