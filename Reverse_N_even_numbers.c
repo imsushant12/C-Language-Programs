37e0c85c80218508d2955aacadace9b476d07587
imsushant12 <sushantgaurav57@gmail.com>
2021-06-07 01:59:09 +0530

    first commit

---
+++ Reverse_N_even_numbers.c
@@ -0,0 +1,12 @@
+main()
+{
+    int N,i;
+    printf("Enter number of terms");
+    scanf("%d",&N);
+    while(N)        //   for(i=N;i>=1;i--)
+    {
+       printf("\n%d",N*2);       // printf("\n%d",i*2);5050
+       N--;
+    }
+
+}
