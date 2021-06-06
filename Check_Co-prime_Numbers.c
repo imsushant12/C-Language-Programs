48033e390e3399b5bac77a3645b912b566ded20e
imsushant12 <sushantgaurav57@gmail.com>
2021-06-07 01:43:36 +0530

    first commit

---
+++ Check_Co-prime_Numbers.c
@@ -0,0 +1,16 @@
+main()
+{
+    int x,y,i,min;
+    printf("Enter two nos\n");
+    scanf("%d %d",&x,&y);
+    min=x<y?x:y;
+    for(i=2;i<=min;i++)
+    {
+        if(x%i==0&&y%i==0)
+            break;
+    }
+    if(i==min+1)
+        printf("Co-prime nos");
+    else
+        printf("Not co-prime nos");
+}
