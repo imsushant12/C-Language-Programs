9cb4ba44aacc7a915a119dffa69a8ea24c64bcfe
imsushant12 <sushantgaurav57@gmail.com>
2021-06-07 02:20:40 +0530

    improved code

--- Check_Co-prime_Numbers.c
+++ Check_Co-prime_Numbers.c
@@ -1,15 +1,17 @@
-main()
+#include <stdio.h>
+#include <stdlib.h>
+void main()
 {
-    int x,y,i,min;
+    int x, y, i, min;
     printf("Enter two nos\n");
-    scanf("%d %d",&x,&y);
-    min=x<y?x:y;
-    for(i=2;i<=min;i++)
+    scanf("%d %d", &x, &y);
+    min = x < y ? x : y;
+    for (i = 2; i <= min; i++)
     {
-        if(x%i==0&&y%i==0)
+        if (x % i == 0 && y % i == 0)
             break;
     }
-    if(i==min+1)
+    if (i == min + 1)
         printf("Co-prime nos");
     else
         printf("Not co-prime nos");
