70a6d647a530e680274d22fb100d6411b0397cfd
imsushant12 <sushantgaurav57@gmail.com>
2021-06-07 01:59:21 +0530

    first commit

---
+++ Reverse_N_odd_numbers.c
@@ -0,0 +1,12 @@
+main()
+{
+    int N,i;
+    printf("Enter number of terms");
+    scanf("%d",&N);
+    while(N)
+    {
+       printf("\n%d",N*2-1);    // can also be written as N*2+i
+       N--;
+    }
+
+}
