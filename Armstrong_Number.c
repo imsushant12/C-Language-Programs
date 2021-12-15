b8ee006cda6f796331ba9b7ff91fa8594272db73
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-12-15 21:39:27 +0530

    first commit

--- Armstrong_Number.c
+++ Armstrong_Number.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void main()
 {
-    int r,x,n,s;
+    int r, x, n, s;
     printf("Armstrong numbers under 1000 are \n");
-    for(n=1;n<=1000;n++)
+    for (n = 1; n <= 1000; n++)
     {
-        s=0;
-        x=n;
-        while(x!=0)
+        s = 0;
+        x = n;
+        while (x != 0)
         {
-            r=x%10;
-            s=s+r*r*r;
-            x=x/10;
+            r = x % 10;
+            s = s + r * r * r;
+            x = x / 10;
         }
-        if(s==n)
-            printf("%d \n",n);
+        if (s == n)
+            printf("%d \n", n);
     }
 }
